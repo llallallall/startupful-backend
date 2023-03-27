7909a043a320c55ae282de55f1da61baadaecd66
hwang myeonghwan <llallallall@kakao.com>
2023-03-28 07:38:43 +0900

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly appService: AppService,
+  ) {}
 
   @Get()
   getHello(): string {
+    //console.log(process.env.DATABASE_HOST);
     return this.appService.getHello();
+  }
+
+  @Get('db-host-from-config')
+  getDatabaseHostFromConfigService(): string {
+    return this.configService.get('DATABASE_HOST');
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+      // process.env.NODE_ENV === 'production'
+      //   ? '.production.env'
+      //   : process.env.NODE_ENV === 'stage'
+      //   ? '.stage.env'
+      //   : '.development.env',
+    }),
+    UsersModule,
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
